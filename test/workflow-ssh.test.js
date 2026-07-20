c1306e56b796179dff095cb6f42d856d035a6a7c
askarahodov <devops.bel@yandex.ru>
2026-07-20 17:58:24 +0300

    Test FreeIPA to SSH user provisioning workflow

--- test/workflow-ssh.test.js
+++ test/workflow-ssh.test.js
@@ -14,40 +14,54 @@ const workflowConfig = JSON.parse(fs.readFileSync(
   'utf8'
 ));
 
-test('SSH plugin is imported separately before the workflow', () => {
-  assert.equal(pluginConfig.items.length, 1);
-  assert.equal(pluginConfig.items[0].type, 'plugin');
-  assert.equal(pluginConfig.items[0].data.id, 'pmlc2ha8fssh1');
+test('SSH plugins are imported separately before the workflow', () => {
+  const pluginIds = pluginConfig.items.map((item) => item.data.id);
+
+  assert.equal(pluginConfig.items.length, 2);
+  assert.ok(pluginConfig.items.every((item) => item.type === 'plugin'));
+  assert.ok(pluginIds.includes('pmlc2ha8fssh1'));
+  assert.ok(pluginIds.includes('pmlc2ha8fssh_user'));
 
   assert.equal(workflowConfig.items.length, 1);
   assert.equal(workflowConfig.items[0].type, 'event');
   assert.equal(workflowConfig.items[0].data.type, 'workflow');
 });
 
-test('workflow creates the user before splitting SSH hosts', () => {
+test('workflow creates the FreeIPA user before provisioning SSH hosts', () => {
   const workflow = workflowConfig.items[0].data;
+  const fields = new Map(workflow.fields.map((field) => [field.id, field]));
   const createNode = workflow.workflow.nodes.find((node) => node.id === 'ncreateipa1');
   const splitNode = workflow.workflow.nodes.find((node) => node.id === 'nsplithost1');
   const sshNode = workflow.workflow.nodes.find((node) => node.id === 'nsshexec01');
   const createToSplit = workflow.workflow.connections.find((conn) => conn.id === 'ccreatesplit');
 
+  assert.equal(fields.get('initial_password').required, true);
+  assert.equal(fields.get('ssh_public_key').required, true);
+  assert.equal(fields.get('grant_sudo').value, true);
+
   assert.equal(createNode.data.plugin, 'pmlc2ha8fipa_create');
   assert.equal(createNode.data.params.uid, '{{ workflow.params.uid }}');
   assert.equal(splitNode.data.split, 'workflow.params.ssh_hosts');
   assert.equal(createToSplit.condition, 'success');
-  assert.equal(sshNode.data.plugin, 'pmlc2ha8fssh1');
+
+  assert.equal(sshNode.data.plugin, 'pmlc2ha8fssh_user');
+  assert.equal(sshNode.data.params.action, 'ssh_provision_user');
   assert.equal(sshNode.data.params.host_port, '{{ data.item }}');
+  assert.equal(sshNode.data.params.username, '{{ workflow.params.uid }}');
+  assert.equal(sshNode.data.params.user_password, '{{ workflow.params.initial_password }}');
+  assert.equal(sshNode.data.params.public_key, '{{ workflow.params.ssh_public_key }}');
   assert.equal(sshNode.data.params.connect_timeout_seconds, '15');
-  assert.match(sshNode.data.params.command, /Hello World/);
 });
 
-test('SSH timeout plugin parameter does not range-validate workflow macros during import', () => {
-  const timeoutParam = pluginConfig.items[0].data.params.find(
-    (param) => param.id === 'connect_timeout_seconds'
-  );
-  assert.ok(timeoutParam);
-  assert.equal(timeoutParam.value, 15);
-  assert.equal(Object.hasOwn(timeoutParam, 'range'), false);
+test('provisioning plugin declares every workflow parameter', () => {
+  const plugin = pluginConfig.items.find((item) => item.data.id === 'pmlc2ha8fssh_user').data;
+  const declared = new Set(plugin.params.map((param) => param.id));
+  const workflow = workflowConfig.items[0].data;
+  const sshNode = workflow.workflow.nodes.find((node) => node.id === 'nsshexec01');
+
+  for (const id of Object.keys(sshNode.data.params)) {
+    assert.ok(declared.has(id), `Missing plugin parameter declaration: ${id}`);
+  }
 });
 
 test('portable workflow has no external tag dependencies', () => {
